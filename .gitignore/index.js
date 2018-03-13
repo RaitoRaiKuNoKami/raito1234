aad28ac517f2b7302fb51f0c4abc4e9c9ef25aff
RaitoRaiKuNoKami <37294455+RaitoRaiKuNoKami@users.noreply.github.com>
2018-03-13 10:08:59 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,12 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoires: [], xp: []}).write()
 
 var prefix = ("*")
 
@@ -8,17 +15,34 @@ bot.on('ready', function() {
     console.log("Connectedç");
 });
 
-bot.login(process.env.TOKEN);
+bot.login("NDIyNjk5Mjk0NzM5Mzk4NjU2.DYfk8g.gVeLtharhv-zlNj06MQ2Pa9kk8I");
 
 
 bot.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n -*help");
-    }
-    
-    if (message.content === "Salut"){
-        message.reply("Bien le bonjour. :)");
-        console.log("Commande Salut effectué");
-    }
-});
-       
+
+    var msgauthor = message.author.id;
+
+    if(message.author.bot)return;
+
+    if(!db.get("xp").find({user: msgauthor}).value()){
+        db.get("xp").push({user: msgauthor, xp: 1}).write();
+    }else{
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        console.log(userxpdb);
+        var user = Object.values(userxpdb)
+        console.log(userxpdb)
+        console.log(`Nombre d'xp: ${userxp[1]}`)
+        
+        db.get("xp").find({user: msgauthor}).assign({user : msgauthor, xp: userxp[1] += 1}).write();
+
+    if (message.content === prefix + "xp"){
+        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+        var xpfinal = Object.values(xp);
+        var xp_embed = new Discord.RichEmbed()
+            .setTitle(`Stat des xp de ${message.author.username}`)
+            .setColor('#aa0a0a')
+            .setDescription("Affichage des XP")
+            .addField("XP:", `${xpfinal[1]} xp`)
+            .setFooter("Enjoy :p")
+        message.channel.send({embed: xp_embed});
+    }}})
